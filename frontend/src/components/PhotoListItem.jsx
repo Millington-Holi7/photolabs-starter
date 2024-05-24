b7819f9f16d4409855f1c0902725656a0a1beca3
Millington-Holi7 <holinas007@gmail.com>
2024-05-24 10:56:31 -0400

    reload all app requirements and move sample code to App.jsx and passed it to the child via props.

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -3,19 +3,18 @@ import React from "react";
 import "../styles/PhotoListItem.scss";
 
 
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
 
-const PhotoListItem = () => {
-  /* Insert React */
+
+const PhotoListItem = (props) => {
+  const {sampleDataForPhotoListItem} = props;
+  return (
+    <div className="PhotoList">
+      <img src={sampleDataForPhotoListItem.imageSource} />
+      <img src={sampleDataForPhotoListItem.profile} />
+      <h5>{sampleDataForPhotoListItem.username}</h5>
+      <h5>{sampleDataForPhotoListItem.location.city}{sampleDataForPhotoListItem.location.country}</h5>
+    </div>
+  )
 };
 
 export default PhotoListItem;
